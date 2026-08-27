3c5b566d62de24d16e25d9972ef815b350a47b5f
Bladimir Huamaccto Mallqui <vdstone20@gmail.com>
2026-08-27 14:07:35 -0500

    Subida de estructura de base de datos y código inicial

---
+++ seguimientoEgresados/src/main/java/com/example/seguimientoEgresados/SeguimientoEgresadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.seguimientoEgresados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoEgresadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoEgresadosApplication.class, args);
+	}
+
+}
---
+++ seguimientoEgresados/src/test/java/com/example/seguimientoEgresados/SeguimientoEgresadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seguimientoEgresados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoEgresadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
